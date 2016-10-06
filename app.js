47c570dd57dc3412b3ea769290b2a30074ddec34
vmanthena <varma.m@mtuity.com>
2016-10-06 10:43:47 +0530

    rangeslider

---
+++ app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by vmanthena on 04-Oct-16.
+ */
+
+angular.module("rangeapp", [])
+    .controller("rangecntrl", ['$scope', function($scope) {
+        $scope.rangeValue = 10;
+    }]);
